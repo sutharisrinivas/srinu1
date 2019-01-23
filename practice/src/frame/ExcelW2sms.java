aaa9f7a50155e7d0604a5e3ec97f592270ae1376
rajukeshoju <rajukeshoju@Kraju>
2019-01-23 14:02:41 +0530

    Excel

---
+++ practice/src/frame/ExcelW2sms.java
@@ -0,0 +1,44 @@
+package frame;
+
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.Number;
+
+public class ExcelW2sms 
+{
+	public static void main(String[] args) throws Exception 
+	{
+	  
+		//create object for file
+		File f=new File("Book1.xls");
+		//open .xls for reading
+		Workbook rwb=Workbook.getWorkbook(f);
+		Sheet rsh=rwb.getSheet(0);
+		int nour=rsh.getRows();
+		//open same .xls file for writing
+		WritableWorkbook wwb=Workbook.createWorkbook(f,rwb);
+		WritableSheet wsh=wwb.getSheet(0);
+		
+		
+		for(int i=1;i<nour;i++)
+		{
+			int x=Integer.parseInt(rsh.getCell(0, i).getContents());
+			int y=Integer.parseInt(rsh.getCell(0,i).getContents());
+			int z=x+y;
+			Number n=new Number(2,i,z);
+			wsh.addCell(n);
+		}
+		//save
+		wwb.write();
+		wwb.close();
+		rwb.close();
+		
+	}
+
+}
